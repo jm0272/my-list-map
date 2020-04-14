a5be41d418046d18f4f892500e2e4f9429326c46
jaemoon.sakong <jm0272@naver.com>
2020-04-15 04:11:54 +0900

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,21 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-app.use((ctx) => {
-    ctx.body = 'hello world123';
-});
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(5000, () => {
-    console.log('listen ...');
+  console.log('listening to port 5000');
 });
